23c46f2397279a4e3dffcf0242a2fcd15fd633de
Arnaud Ritti <arnaud.ritti@adeliom.com>
2015-09-06 21:52:10 +0200

    New version

--- dist/jquery.thorslider.js
+++ dist/jquery.thorslider.js
@@ -6,11 +6,16 @@
  *  Made by Arnaud Ritti
  *  Under MIT License
  */
+/* Modernizr 2.6.2 (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-csstransitions-touch-shiv-cssclasses-prefixed-teststyles-testprop-testallprops-prefixes-domprefixes-load
+ */
+
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
 
 	"use strict";
+
 		// undefined is used here as the undefined global variable in ECMAScript 3 is
 		// mutable (ie. it can be changed by someone else). undefined isn"t really being
 		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
@@ -24,206 +29,212 @@
 		var pluginName = "thorSlider",
 				defaults = {
 					speed: 800,
-					infinite: true,
-					easing: "linear",
-					fade: false,
-					dots: true,
-					arrows: true,
-					nextArrow: "thor-next",
-					prevArrow: "thor-prev",
-					upArrow: "thor-up",
-					downArrow: "thor-down",
-					autoplay: false,
-					autoplaySpeed: 1000,
-					touch: true,
-					pauseOnHover: true
-		};
+					autoplay: true,
+					autoplaySpeed: 2000,
+					arrows_bg : false
+				};
 
-		var self;
+				var	support = Modernizr.csstransitions,
+	// transition end event
+	// https://github.com/twitter/bootstrap/issues/2870
+	transEndEventNames = {
+		"WebkitTransition" : "webkitTransitionEnd",
+		"MozTransition" : "transitionend",
+		"OTransition" : "oTransitionEnd",
+		"msTransition" : "MSTransitionEnd",
+		"transition" : "transitionend"
+	},
+	// its name
+	transEndEventName = transEndEventNames[ Modernizr.prefixed( "transition" ) ];
+	var _ = this;
 
 		// The actual plugin constructor
 		function ThorSlider ( element, options ) {
-
-
 				this.element = element;
 				// jQuery has an extend method which merges the contents of two or
 				// more objects, storing the result in the first object. The first object
 				// is generally empty as we don"t want to alter the default options for
 				// future instances of the plugin
-				this.settings = jQuery.extend( {}, defaults, options );
+				this.settings = $.extend( {}, defaults, options );
 				this._defaults = defaults;
 				this._name = pluginName;
 
-				this.initials = {
-	        currSlide : 0,
-	        $currSlide: null,
-	        totalSlides : false,
-	      };
-				jQuery("body")
-					.on("click","."+this.settings.nextArrow+"",{direction:"right"},this.changeSlide)
-					.on("click","."+this.settings.prevArrow+"",{direction:"left"},this.changeSlide)
-					.on("click","."+this.settings.upArrow+"",{direction:"top"},this.changeSlide)
-					.on("click","."+this.settings.downArrow+"",{direction:"bottom"},this.changeSlide)
-					.on("click",".thor-dots li",this.changeSlide);
+
+				this.$container = $(this.element),
+				this.$contentwrapper = this.$container.children( "div.ts-contentwrapper" ),
+				// the items (description elements for the slides/products)
+				this.$items = this.$contentwrapper.children( "div.ts-content" ),
+				this.itemsCount = this.$items.length,
+				this.$slidewrapper = this.$container.children( "div.ts-slidewrapper" ),
+				// the slides (product images)
+				this.$slidescontainer = this.$slidewrapper.find( "div.ts-slides" ),
+				this.$slides = this.$slidescontainer.children( "div" ),
+				// navigation arrows
+				this.$navprev = this.$slidewrapper.find( "nav > a.ts-prev" ),
+				this.$navnext = this.$slidewrapper.find( "nav > a.ts-next" ),
+				// this.current index for items and slides
+				this.current = 0,
+				// checks if the transition is in progress
+				this.isAnimating = false;
+
 
 				this.init();
-
 		}
 
-		var _next = function (e,direction) {
-			// If the event was triggered by a slide indicator, we store the data-index value of that indicator
-			var index = (typeof e !== "undefined" ? jQuery(e.currentTarget).data("index") : undefined);
-
-			//Logic for determining the next slide
-			switch(true){
-				//If the event was triggered by an indicator, we set the next slide based on index
-				 case( typeof index !== "undefined"):
-					if (self.currSlide === index){
-						self.startTimer();
-						return false;
-					}
-					self.currSlide = index;
-				break;
-				case(direction === "right" && self.currSlide < (self.totalSlides - 1)):
-					self.currSlide++;
-				break;
-				case(direction === "right"):
-					self.currSlide = 0;
-				break;
-				case(direction === "top" && self.currSlide < (self.totalSlides - 1)):
-					self.currSlide++;
-				break;
-				case(direction === "top"):
-					self.currSlide = 0;
-				break;
-				case(direction === "left" && self.currSlide === 0):
-					self.currSlide = (self.totalSlides - 1);
-				break;
-				case(direction === "left"):
-					self.currSlide--;
-				break;
-				case(direction === "bottom" && self.currSlide === 0):
-					self.currSlide = (self.totalSlides - 1);
-				break;
-				case(direction === "bottom"):
-					self.currSlide--;
-				break;
-			}
-			return true;
-		};
-
 		// Avoid Plugin.prototype conflicts
-		jQuery.extend(ThorSlider.prototype, {
+		$.extend(ThorSlider.prototype, {
 				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. self.element
-						// and this.settings
-						// you can add more functions like the one below and
-						// call them like so: self.yourOtherFunction(self.element, this.settings).
-						self = this;
-						this.$el = jQuery(this.element);
-						this.$el.addClass("thor-slider");
 
-						if(this.settings.dots){
-							var indicators = this.$el.append("<ul class=\"thor-dots\" >").find(".thor-dots");
-							this.totalSlides = this.$el.find(".thor-slide").length;
-							for(var i = 0; i < this.totalSlides; i++) {indicators.append("<li data-index=\""+i+"\" "+(i === 0 ? "class=\"active\"" : "")+">");}
+					_ = this;
+
+					// show first item
+						var $currentItem = _.$items.eq( this.current ),
+							$currentSlide = _.$slides.eq( this.current ),
+							initCSS = {
+								top : 0,
+								zIndex : 999
+							};
+						$currentItem.css( initCSS );
+						$currentSlide.css( initCSS );
+
+						if(_.settings.autoplay){
+
+							_.initTimer();
 						}
 
-						if(this.settings.arrows){
-							var arrows = self.$el.append("<div class=\"thor-arrows\" >").find(".thor-arrows");
-							self.totalSlides = self.$el.find(".thor-slide").length;
-							arrows.append("<button class=\""+this.settings.upArrow+"\">Up</button>");
-							arrows.append("<button class=\""+this.settings.prevArrow+"\">Prev</button>");
-							arrows.append("<button class=\""+this.settings.nextArrow+"\">Next</button>");
-							arrows.append("<button class=\""+this.settings.downArrow+"\">Down</button>");
-						}
+						// update nav images
+						_.updateNavImages();
 
-						if(this.settings.autoplay){
-							this.startTimer();
-						}
-
-
-						this.currSlide = 0;
-						this.$currSlide = this.$el.find(".thor-slide").eq(0);
-						this.$currSlide.addClass("active");
+						// initialize some events
+						_.initEvents();
 				},
 				initTimer: function () {
-					if (this.timer) {clearInterval(this.timer);}
-					this.timer = setInterval(this.changeSlide, this.settings.autoplaySpeed);
+					_.timer = setInterval(function() {
+						_.slide("next");
+					}, _.settings.autoplaySpeed);
 				},
 				startTimer: function () {
-					this.initTimer();
-					this.throttle = false;
+					_.initTimer();
+					_.throttle = false;
 				},
 				clearTimer: function () {
-					if (this.timer) {clearInterval(this.timer);}
+					if (_.timer) {clearInterval(_.timer);}
 				},
-				changeSlide: function (e) {
+				updateNavImages : function () {
 
-					//Ensure that animations are triggered one at a time
-					if (self.throttle) {return;}
-					this.throttle = true;
 
-					if(self.settings.autoplay){
-						//Stop the timer as the animation is getting carried out
-						if (this.timer) {this.clearTimer();}
+					if(_.settings.arrows_bg){
+						// updates the background image for the navigation arrows
+						var configPrev = ( _.current > 0 ) ? _.$slides.eq( _.current - 1 ).css( "background-image" ) : _.$slides.eq( _.itemsCount - 1 ).css( "background-image" ),
+							configNext = ( _.current < _.itemsCount - 1 ) ? _.$slides.eq( _.current + 1 ).css( "background-image" ) : _.$slides.eq( 0 ).css( "background-image" );
+
+						_.$navprev.css( "background-image", configPrev );
+						_.$navnext.css( "background-image", configNext );
 					}
-
-
-					// Returns the animation direction (left or right)
-					var dir;
-					if (typeof e !== "undefined"){dir = (typeof e.data === "undefined" ? "right" : e.data.direction);
-				} else {dir = "right";}
-
-					// Selects the next slide
-					var animate = _next(e,dir);
-					if (!animate) {return;}
-
-					//Active the next slide to scroll into view
-					var $nextSlide = self.$el.find(".thor-slide").eq(self.currSlide);
-					$nextSlide.addClass(dir + " active");
-					self._jsAnimation($nextSlide,dir);
 				},
-				_jsAnimation: function ($nextSlide,direction) {
-					//Cache this reference for use inside animate functions
-
-					var _ = this;
-
-
-			     var animation = {};
-					animation[direction] = "0%";
-
-					var animationPrev = {};
-					animationPrev[direction] = "100%";
-
-					//Animation: Current slide
-					self.$currSlide.animate({opacity: "0"},self.settings.speed);
-					//Animation: Next slide
-					$nextSlide.animate({opacity: "1"},this.settings.speed,"swing",function(){
-
-						//Get rid of any JS animation residue
-						self.$currSlide.removeClass("active");
-						//Cache the next slide after classes and inline styles have been removed
-						self.$currSlide = $nextSlide.removeClass(direction);
-						_._updateIndicators();
-						if(_.settings.autoplay){
-							_.startTimer();
+				initEvents : function () {
+					_.$navprev.on( "click", function( ) {
+						if( !_.isAnimating ) {
+							_.slide( "prev" );
 						}
-					});
+						return false;
+					} );
+
+					_.$navnext.on( "click", function( ) {
+						if( !_.isAnimating ) {
+							_.slide( "next" );
+						}
+						return false;
+					} );
+
+
+/*
+
+*/
+					// transition end event
+					_.$items.on( transEndEventName, _.removeTransition );
+					_.$slides.on( transEndEventName, _.removeTransition );
 				},
-				_updateIndicators: function () {
-					self.$el.find(".thor-dots li").removeClass("active").eq(self.currSlide).addClass("active");
+				removeTransition : function () {
+					_.isAnimating = false;
+					$(this).removeClass("ts-move");
+				},
+				slide : function (dir) {
+
+					_.startTimer();
+					_.isAnimating = true;
+
+
+					var $currentItem = _.$items.eq( _.current ),
+						$currentSlide = _.$slides.eq( _.current );
+
+					// update this.current value
+					if( dir === "next" ) {
+						( _.current < _.itemsCount - 1 ) ? ++_.current : _.current = 0;
+					}
+					else if( dir === "prev" ) {
+						( _.current > 0 ) ? --_.current : _.current = _.itemsCount - 1;
+					}
+						// new item that will be shown
+					var $newItem = _.$items.eq( _.current ),
+						// new slide that will be shown
+						$newSlide = _.$slides.eq( _.current );
+
+					// position the new item up or down the viewport depending on the direction
+					$newItem.css( {
+						top : ( dir === "next" ) ? "-100%" : "100%",
+						zIndex : 999
+					} );
+
+					$newSlide.css( {
+						top : ( dir === "next" ) ? "100%" : "-100%",
+						zIndex : 999
+					} );
+
+					setTimeout( function() {
+
+						// move the this.current item and slide to the top or bottom depending on the direction
+						$currentItem.addClass( "ts-move" ).css( {
+							top : ( dir === "next" ) ? "100%" : "-100%",
+							zIndex : 1
+						} );
+
+						$currentSlide.addClass( "ts-move" ).css( {
+							top : ( dir === "next" ) ? "-100%" : "100%",
+							zIndex : 1
+						} );
+
+						// move the new ones to the main viewport
+						$newItem.addClass( "ts-move" ).css( "top", 0 );
+						$newSlide.addClass( "ts-move" ).css( "top", 0 );
+
+						// if no CSS transitions set the isAnimating flag to false
+						if( !support ) {
+							isAnimating = false;
+						}
+
+
+
+						// update nav images
+						_.updateNavImages();
+
+						_.clearTimer();
+
+					}, 0 );
+
+
+
+
+
 				}
 		});
 
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
-		jQuery.fn[ pluginName ] = function ( options ) {
+		$.fn[ pluginName ] = function ( options ) {
 				return this.each(function() {
-						if ( !jQuery.data( this, "plugin_" + pluginName ) ) {
-								jQuery.data( this, "plugin_" + pluginName, new ThorSlider( this, options ) );
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new ThorSlider( this, options ) );
 						}
 				});
 		};

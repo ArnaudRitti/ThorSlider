229e00e0776ea3b3d5db54843692fa28f666152a
Arnaud Ritti <arnaud.ritti@adeliom.com>
2015-09-06 16:20:28 +0200

    Update 6/9/15

--- src/jquery.thorslider.js
+++ src/jquery.thorslider.js
@@ -3,7 +3,6 @@
 ;(function ( $, window, document, undefined ) {
 
 	"use strict";
-
 		// undefined is used here as the undefined global variable in ECMAScript 3 is
 		// mutable (ie. it can be changed by someone else). undefined isn"t really being
 		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
@@ -16,7 +15,7 @@
 		// Create the defaults once
 		var pluginName = "thorSlider",
 				defaults = {
-					speed: 300,
+					speed: 800,
 					infinite: true,
 					easing: "linear",
 					fade: false,
@@ -24,20 +23,26 @@
 					arrows: true,
 					nextArrow: "thor-next",
 					prevArrow: "thor-prev",
+					upArrow: "thor-up",
+					downArrow: "thor-down",
 					autoplay: false,
-					autoplaySpeed: 3000,
+					autoplaySpeed: 1000,
 					touch: true,
 					pauseOnHover: true
 		};
 
+		var self;
+
 		// The actual plugin constructor
-		function Plugin ( element, options ) {
+		function ThorSlider ( element, options ) {
+
+
 				this.element = element;
 				// jQuery has an extend method which merges the contents of two or
 				// more objects, storing the result in the first object. The first object
 				// is generally empty as we don"t want to alter the default options for
 				// future instances of the plugin
-				this.settings = $.extend( {}, defaults, options );
+				this.settings = jQuery.extend( {}, defaults, options );
 				this._defaults = defaults;
 				this._name = pluginName;
 
@@ -46,38 +51,98 @@
 	        $currSlide: null,
 	        totalSlides : false,
 	      };
+				jQuery("body")
+					.on("click","."+this.settings.nextArrow+"",{direction:"right"},this.changeSlide)
+					.on("click","."+this.settings.prevArrow+"",{direction:"left"},this.changeSlide)
+					.on("click","."+this.settings.upArrow+"",{direction:"top"},this.changeSlide)
+					.on("click","."+this.settings.downArrow+"",{direction:"bottom"},this.changeSlide)
+					.on("click",".thor-dots li",this.changeSlide);
 
 				this.init();
+
 		}
 
+		var _next = function (e,direction) {
+			// If the event was triggered by a slide indicator, we store the data-index value of that indicator
+			var index = (typeof e !== "undefined" ? jQuery(e.currentTarget).data("index") : undefined);
+
+			//Logic for determining the next slide
+			switch(true){
+				//If the event was triggered by an indicator, we set the next slide based on index
+				 case( typeof index !== "undefined"):
+					if (self.currSlide === index){
+						self.startTimer();
+						return false;
+					}
+					self.currSlide = index;
+				break;
+				case(direction === "right" && self.currSlide < (self.totalSlides - 1)):
+					self.currSlide++;
+				break;
+				case(direction === "right"):
+					self.currSlide = 0;
+				break;
+				case(direction === "top" && self.currSlide < (self.totalSlides - 1)):
+					self.currSlide++;
+				break;
+				case(direction === "top"):
+					self.currSlide = 0;
+				break;
+				case(direction === "left" && self.currSlide === 0):
+					self.currSlide = (self.totalSlides - 1);
+				break;
+				case(direction === "left"):
+					self.currSlide--;
+				break;
+				case(direction === "bottom" && self.currSlide === 0):
+					self.currSlide = (self.totalSlides - 1);
+				break;
+				case(direction === "bottom"):
+					self.currSlide--;
+				break;
+			}
+			return true;
+		};
+
 		// Avoid Plugin.prototype conflicts
-		$.extend(Plugin.prototype, {
+		jQuery.extend(ThorSlider.prototype, {
 				init: function () {
 						// Place initialization logic here
 						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
+						// the options via the instance, e.g. self.element
 						// and this.settings
 						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.settings).
-						console.log(this.element);
-						this.$el = $(this.element);
+						// call them like so: self.yourOtherFunction(self.element, this.settings).
+						self = this;
+						this.$el = jQuery(this.element);
 						this.$el.addClass("thor-slider");
 
 						if(this.settings.dots){
-							var indicators = this.$el.append("<ul class=\"thor-dots\" >").find(".thor-slide");
+							var indicators = this.$el.append("<ul class=\"thor-dots\" >").find(".thor-dots");
 							this.totalSlides = this.$el.find(".thor-slide").length;
-							for(var i = 0; i < this.totalSlides; i++) {indicators.append("<li data-index=\""+i+"\">");}
+							for(var i = 0; i < this.totalSlides; i++) {indicators.append("<li data-index=\""+i+"\" "+(i === 0 ? "class=\"active\"" : "")+">");}
 						}
 
-						$("body")
-							.on("click",this.settings.nextArrow,{direction:"right"},this.changeSlide)
-							.on("click",this.settings.prevArrow,{direction:"left"},this.changeSlide)
-							.on("click",".thor-dots li",this.changeSlide);
+						if(this.settings.arrows){
+							var arrows = self.$el.append("<div class=\"thor-arrows\" >").find(".thor-arrows");
+							self.totalSlides = self.$el.find(".thor-slide").length;
+							arrows.append("<button class=\""+this.settings.upArrow+"\">Up</button>");
+							arrows.append("<button class=\""+this.settings.prevArrow+"\">Prev</button>");
+							arrows.append("<button class=\""+this.settings.nextArrow+"\">Next</button>");
+							arrows.append("<button class=\""+this.settings.downArrow+"\">Down</button>");
+						}
 
+						if(this.settings.autoplay){
+							this.startTimer();
+						}
+
+
+						this.currSlide = 0;
 						this.$currSlide = this.$el.find(".thor-slide").eq(0);
-						this.$el.find(".thor-dots li").eq(0).addClass("active");
+						this.$currSlide.addClass("active");
 				},
 				initTimer: function () {
+					if (this.timer) {clearInterval(this.timer);}
 					this.timer = setInterval(this.changeSlide, this.settings.autoplaySpeed);
 				},
 				startTimer: function () {
@@ -88,71 +153,36 @@
 					if (this.timer) {clearInterval(this.timer);}
 				},
 				changeSlide: function (e) {
+
 					//Ensure that animations are triggered one at a time
-					if (this.throttle) {return;}
+					if (self.throttle) {return;}
 					this.throttle = true;
 
-					//Stop the timer as the animation is getting carried out
-					this.clearTimer();
+					if(self.settings.autoplay){
+						//Stop the timer as the animation is getting carried out
+						if (this.timer) {this.clearTimer();}
+					}
+
 
 					// Returns the animation direction (left or right)
-					var direction = this._direction(e);
+					var dir;
+					if (typeof e !== "undefined"){dir = (typeof e.data === "undefined" ? "right" : e.data.direction);
+				} else {dir = "right";}
 
 					// Selects the next slide
-					var animate = this._next(e,direction);
+					var animate = _next(e,dir);
 					if (!animate) {return;}
 
 					//Active the next slide to scroll into view
-					var $nextSlide = this.$el.find(".slide").eq(this.currSlide).addClass(direction + " active");
-
-					this._jsAnimation($nextSlide,direction);
-				},
-				_direction: function(e){
-					var direction;
-
-					// Default to forward movement
-					if (typeof e !== "undefined"){
-						direction = (typeof e.data === "undefined" ? "right" : e.data.direction);
-					} else {
-						direction = "right";
-					}
-					return direction;
-				},
-				_next: function (e,direction) {
-					// If the event was triggered by a slide indicator, we store the data-index value of that indicator
-					var index = (typeof e !== "undefined" ? $(e.currentTarget).data("index") : undefined);
-
-					//Logic for determining the next slide
-					switch(true){
-						//If the event was triggered by an indicator, we set the next slide based on index
-						 case( typeof index !== "undefined"):
-							if (this.currSlide === index){
-								this.startTimer();
-								return false;
-							}
-							this.currSlide = index;
-						break;
-						case(direction === "right" && this.currSlide < (this.totalSlides - 1)):
-							this.currSlide++;
-						break;
-						case(direction === "right"):
-							this.currSlide = 0;
-						break;
-						case(direction === "left" && this.currSlide === 0):
-							this.currSlide = (this.totalSlides - 1);
-						break;
-						case(direction === "left"):
-							this.currSlide--;
-						break;
-					}
-					return true;
+					var $nextSlide = self.$el.find(".thor-slide").eq(self.currSlide);
+					$nextSlide.addClass(dir + " active");
+					self._jsAnimation($nextSlide,dir);
 				},
 				_jsAnimation: function ($nextSlide,direction) {
 					//Cache this reference for use inside animate functions
+
 					var _ = this;
 
-			     // See CSS for explanation of .js-reset-left
-					if(direction === "right") {_.$currSlide.addClass("js-reset-left");}
 
 			     var animation = {};
 					animation[direction] = "0%";
@@ -161,29 +191,31 @@
 					animationPrev[direction] = "100%";
 
 					//Animation: Current slide
-					this.$currSlide.animate(animationPrev,this.settings.speed);
-
+					self.$currSlide.animate({opacity: "0"},self.settings.speed);
 					//Animation: Next slide
-					$nextSlide.animate(animation,this.settings.speed,"swing",function(){
+					$nextSlide.animate({opacity: "1"},this.settings.speed,"swing",function(){
+
 						//Get rid of any JS animation residue
-						_.$currSlide.removeClass("active js-reset-left").attr("style","");
+						self.$currSlide.removeClass("active");
 						//Cache the next slide after classes and inline styles have been removed
-						_.$currSlide = $nextSlide.removeClass(direction).attr("style","");
+						self.$currSlide = $nextSlide.removeClass(direction);
 						_._updateIndicators();
-						_.startTimer();
+						if(_.settings.autoplay){
+							_.startTimer();
+						}
 					});
 				},
 				_updateIndicators: function () {
-					this.$el.find(".indicators li").removeClass("active").eq(this.currSlide).addClass("active");
+					self.$el.find(".thor-dots li").removeClass("active").eq(self.currSlide).addClass("active");
 				}
 		});
 
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
-		$.fn[ pluginName ] = function ( options ) {
+		jQuery.fn[ pluginName ] = function ( options ) {
 				return this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						if ( !jQuery.data( this, "plugin_" + pluginName ) ) {
+								jQuery.data( this, "plugin_" + pluginName, new ThorSlider( this, options ) );
 						}
 				});
 		};
